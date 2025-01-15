9ff1da8e3ff17e0f390c9529178a99be17b65383
Souvik Sanyal <souvikavro00.sa@gmail.com>
2025-01-15 09:06:13 +0600

    repository added for post entity ,solve the issue #4

---
+++ src/main/java/com/example/lostnfound/repository/PostRepo.java
@@ -0,0 +1,8 @@
+package com.example.lostnfound.repository;
+
+import com.example.lostnfound.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepo extends JpaRepository<Post, Integer>{
+
+}  
