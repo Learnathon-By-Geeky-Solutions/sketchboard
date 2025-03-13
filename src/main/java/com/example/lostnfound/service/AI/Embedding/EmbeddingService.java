9023df2a57f3d53271734d8701dda1636b80feeb
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-03-13 08:17:50 +0600

    fixed rebase

--- src/main/java/com/example/lostnfound/service/AI/Embedding/EmbeddingService.java
+++ src/main/java/com/example/lostnfound/service/AI/Embedding/EmbeddingService.java
@@ -2,51 +2,57 @@ package com.example.lostnfound.service.AI.Embedding;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpHeaders;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 @Data
 public class EmbeddingService {
 
-    public EmbeddingService() {
-
-    }
-
     /*
-            import requests
+       import requests
 
-            API_URL = "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2"
-            headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxx"}
+hf_token = ""
 
-            def query(payload):
-                response = requests.post(API_URL, headers=headers, json=payload)
-                return response.json()
+API_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+headers = {"Authorization": f"Bearer {hf_token}"}
 
-            output = query({
-                "inputs": {
-                "source_sentence": "That is a happy person",
-                "sentences": [
-                    "That is a happy dog",
-                    "That is a very happy person",
-                    "Today is a sunny day"
-                ]
-            },
-            })
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+output = query({"inputs": "The big brown dog"})
+
+print(output)
+
 
          */
     // Make http request
     @Value("${gemini.api.key}")
     private String huggingfaceToken;
-    String url = "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2";
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .header("Authorization", "Bearer " + huggingfaceToken)
-            .build();
+    String url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Get response
+    public float[] getEmbedding(String input) throws IOException, InterruptedException {
+        System.out.println("Getting embedding for: " + input);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Authorization", "Bearer " + huggingfaceToken)
+                .headers("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("(\"{\\\"inputs\\\": \\\"\" + input + \"\\\"}\")"))
+                .build();
+
+        // Get response
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        return objectMapper.readValue(response.body(), float[][].class)[0];
+    }
 
 
 }
