91cfecd7d979b013aa0a4a89b18e3d8c9001b229
Sanjoy Kumar <sanjoy.ru.cse@gmail.com>
2025-03-16 20:00:58 +0600

    Issue #88 is resolved. All endpoints are returning ResponseEntity of DTO.

--- src/main/java/com/example/lostnfound/controller/AISearchController.java
+++ src/main/java/com/example/lostnfound/controller/AISearchController.java
@@ -11,11 +11,13 @@ import com.example.lostnfound.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AISearchController {
@@ -35,41 +37,41 @@ public class AISearchController {
 
         @PostMapping("/basicAISearch")
         @Operation(summary = "Basic AI Search", description = "Searches for posts using AI")
-        public ResponseEntity<List<Post>> basicAISearch(@RequestBody String query) {
+        public ResponseEntity<List<PostDto>> basicAISearch(@RequestBody String query) {
                 String response = geminiResponse.getResponse(query);
-                return queryExecutor.executeAISearch(response);
+                ResponseEntity<List<Post>> result = queryExecutor.executeAISearch(response);
+                List<PostDto> postDtos = result.getBody().stream()
+                        .map(post -> modelMapper.map(post, PostDto.class))
+                        .collect(Collectors.toList());
+                return new ResponseEntity<>(postDtos, result.getStatusCode());
         }
 
         @PostMapping("/getEmbedding")
         @Operation(summary = "Get Embedding", description = "Get embedding for a given input")
-        public float[] getEmbedding(@RequestBody String input) throws Exception {
-                return embeddingService.getEmbedding(input);
+        public ResponseEntity<float[]> getEmbedding(@RequestBody String input) throws Exception {
+                float[] embedding = embeddingService.getEmbedding(input);
+                return new ResponseEntity<>(embedding, HttpStatus.OK);
         }
 
         @PostMapping("/enhancedSearch")
         @Operation(summary = "Enhanced Search", description = "Searches for posts using AI and returns similar posts")
-        public List<PostDto> enhancedSearch(@RequestBody aiSearchQuery query) throws IOException, InterruptedException {
+        public ResponseEntity<List<PostDto>> enhancedSearch(@RequestBody aiSearchQuery query) throws IOException, InterruptedException {
                 float[] queryEmbedding = embeddingService.getEmbedding(query.getQuery());
-                System.out.println("Query embedding: " + queryEmbedding);
                 List<Post> res = postService.findTopKSimilarPosts(queryEmbedding, query.getLimit());
-                //use object mapper
-                List<PostDto> postDtos = new java.util.ArrayList<>(List.of());
-                for (Post p : res) {
-                        postDtos.add(modelMapper.map(p, PostDto.class));
-                }
-                return postDtos;
+                List<PostDto> postDtos = res.stream()
+                        .map(post -> modelMapper.map(post, PostDto.class))
+                        .collect(Collectors.toList());
+                return new ResponseEntity<>(postDtos, HttpStatus.OK);
         }
 
         @PostMapping("/getSimilarPosts")
         @Operation(summary = "Get Similar Posts", description = "Get similar posts for a given post")
-        public List<PostDto> getSimilarPosts(@RequestBody aiSearchQuery query) {
+        public ResponseEntity<List<PostDto>> getSimilarPosts(@RequestBody aiSearchQuery query) {
                 Post post = postService.getPost(query.getPostId());
                 List<Post> res = postService.findTopKSimilarPosts(post.getEmbedding(), query.getLimit());
-                //use object mapper
-                List<PostDto> postDtos = new java.util.ArrayList<>(List.of());
-                for (Post p : res) {
-                        postDtos.add(modelMapper.map(p, PostDto.class));
-                }
-                return postDtos;
+                List<PostDto> postDtos = res.stream()
+                        .map(p -> modelMapper.map(p, PostDto.class))
+                        .collect(Collectors.toList());
+                return new ResponseEntity<>(postDtos, HttpStatus.OK);
         }
 }
