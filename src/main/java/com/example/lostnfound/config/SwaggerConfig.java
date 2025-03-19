3f6b56ac1aac52dbed2a942263f844b72acc2e36
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-03-19 14:49:07 +0600

    added swagger header

---
+++ src/main/java/com/example/lostnfound/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example.lostnfound.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("LostNFound API Documentation").version("1.0").description(" by team Sketchboard")
+                );
+    }
+}
