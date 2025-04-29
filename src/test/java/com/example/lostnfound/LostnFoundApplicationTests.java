b1b7ef2586e595eb24a2670cefbfdd17e4f8542e
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-04-29 21:16:28 +0600

    better jacoco

--- src/test/java/com/example/lostnfound/LostnFoundApplicationTests.java
+++ src/test/java/com/example/lostnfound/LostnFoundApplicationTests.java
@@ -5,12 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
-@Disabled("No test cases in this project till now")
 class LostnFoundApplicationTests {
 	
 	@Test
 	void contextLoads() {
 		//There will be no test cases in this project till now
+
 	}
 
 }
