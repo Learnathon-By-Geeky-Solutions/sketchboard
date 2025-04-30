b09f5fb1df1b0c7b4c7040fd76f0efd3c367ff65
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-04-30 19:05:58 +0600

    sonar config update

--- src/test/java/com/example/lostnfound/LostnFoundApplicationTests.java
+++ src/test/java/com/example/lostnfound/LostnFoundApplicationTests.java
@@ -2,10 +2,8 @@ package com.example.lostnfound;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
-@Disabled("Disabled until we have test cases")
 class LostnFoundApplicationTests {
 	
 	@Test
